d6c709bc33e882bc6c8d54e177dfbf1cd434f876
Ivona Ze <zelinkova.ivona@gmail.com>
2020-11-22 16:59:13 +0100

    new component

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import './index.html';
@@ -6,6 +6,8 @@ import { countries } from './countries.js';
 
 const App = () => {
   const [country, setCountry] = useState('CZ');
+  // const [countryOption, setCountryOption] = useState('CZ');
+  const [editMode, setEditMode] = useState(false);
 
   const handleCountryChange = (e) => {
     setCountry(e.target.value);
@@ -14,27 +16,36 @@ const App = () => {
   return (
     <div className="container">
       <div className="country-info">
+        {/* 1. nejdřív jde vidět div s jednou zemí, div options, input text uživ. filtr jsou schované
+2. kliknu do divu s jednou zemí, schová se div s jednou zemí, objeví se input text
+3. uživatel píše do input textu, na změnu textu (on change) se mění seznam států (div options)
+4. uživatel klikne na stát, ten se objeví v divu země, schová se zbytek */}
         <form>
-          <div className="form-field">
-            <label htmlFor="country-select">Country:</label>
-            <select
-              id="country-select"
-              value={country}
-              onChange={handleCountryChange}
-            >
-              {countries.map((country) => (
-                <option value={country.country}>{country.name}</option>
-              ))}
+          <div>
+            {!editMode && (
+              <div onClick={() => setEditMode(true)}>vybrano je: {country}</div>
+            )}
 
-              {/* <option value="AF">Afghanistan</option>
-              <option value="CZ">Czech Republic</option>
-              <option value="BD">Bangladesh</option> 
-              
-              chci input text, jeho obsah použiju na filtrování pole countries (use effect)
-              
-              */}
-            </select>
+            {editMode && (
+              <input type="text" placeholder="Enter item to be searched" />
+            )}
           </div>
+          {editMode && (
+            <div className="country-list">
+              {countries.map((item) => (
+                <div
+                  key={item.name}
+                  value={item.country}
+                  onClick={() => {
+                    setEditMode(false);
+                    setCountry(item.name);
+                  }}
+                >
+                  {item.name}
+                </div>
+              ))}
+            </div>
+          )}
         </form>
       </div>
     </div>
